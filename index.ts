f744c2d156074941cf388cd2fb67044d91e4ae3f
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2018-06-19 20:29:31 -0400

    :hammer: Optimize imports

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
-import { createProbot } from 'probot';
 import * as dotenv from 'dotenv';
-import robot from "./src/robot";
+import { createProbot } from 'probot';
 import { readFileSync } from 'fs';
 
+import robot from './src/robot';
+
+// Allow use of .env
 dotenv.config();
 
-// Set up Probot App
+// Create Probot App
 const probot = createProbot({
-	id: Number(process.env.APP_ID),
-	secret: process.env.WEBHOOK_SECRET,
-	port: Number(process.env.APP_PORT),
-	webhookProxy: process.env.WEBHOOK_PROXY_URL,
-	cert: readFileSync('private_key.pem') as any
+  id: Number(process.env.APP_ID),
+  secret: process.env.WEBHOOK_SECRET,
+  port: Number(process.env.APP_PORT),
+  webhookProxy: process.env.WEBHOOK_PROXY_URL,
+  cert: readFileSync('private_key.pem') as any,
 });
 
-// 
+// Set up with robot listener
 probot.setup([robot]);
 probot.start();
