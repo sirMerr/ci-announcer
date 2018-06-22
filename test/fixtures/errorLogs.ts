62f0c1732512bfc85de0555251c7d134c569936f
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2018-06-22 01:16:04 -0400

    :sparkles: Add fixtures

---
+++ test/fixtures/errorLogs.ts
@@ -0,0 +1,17 @@
+export const mockErrorLog = ` FAIL  ./fail.test.js
+  ✕ mistakes 1 + 2 to equal 4 (13ms)
+
+  ● mistakes 1 + 2 to equal 4
+
+    expect(received).toBe(expected) // Object.is equality
+
+    Expected: 4
+    Received: 3
+
+      1 | test('mistakes 1 + 2 to equal 4', () => {
+    > 2 | 	expect(1 + 2).toBe(4);
+        | 	              ^
+      3 | });
+      4 | 
+
+      at Object.<anonymous>.test (fail.test.js:2:16)`;
